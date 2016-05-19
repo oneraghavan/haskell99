3e05a019a4cf74c8fc5b1a1f484c50df86c72626
Raghavan <oneraghavan@gmail.com>
2016-05-19 20:32:04 +0530

    solutions for problem 4 and 5

--- haskell99/src/Problems1to10.hs
+++ haskell99/src/Problems1to10.hs
@@ -14,3 +14,12 @@ myButLast (x:xs) = myButLast xs
 elementAt :: [a] -> Int -> a
 elementAt (x:xs) 1 = x
 elementAt (x:xs) c = elementAt xs (c - 1)
+
+myLength :: [a] -> Int
+myLength [] = 0
+myLength (x:[]) = 1
+myLength (x:xs) = 1 + myLength xs
+
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x:xs) = myReverse xs ++ [x]
--- haskell99/test/TestProblem1to10.hs
+++ haskell99/test/TestProblem1to10.hs
@@ -34,9 +34,22 @@ shouldTestElementAt =
           TestCase  ( assertEqual "should return the actual element at" 9 ( elementAt [1,7,1,9] 4))
          ]
 
+shouldTestmyLength =
+        [
+          TestCase  ( assertEqual "should return the actual lenght of the array" 4 ( myLength [1,2,7,1])),
+          TestCase  ( assertEqual "should return 0 for empty array" 0 ( myLength []))
+         ]
+
+shouldTestmyReverse =
+        [
+          TestCase  ( assertEqual "should reverse the  array" ([1,7,2,1]) ( myReverse [1,2,7,1])),
+          TestCase  ( assertEqual "should return revesre of array" ("edcba") ( myReverse "abcde")),
+          TestCase  ( assertEqual "should return empty array for empty array" ([]) ( myReverse ([]::[Int])))
+         ]
+
 main :: IO ()
 main = do
-    cs@(Counts _ _ errs fails) <- runTestTT $ TestList ( shouldTestMylast ++ shouldTestMyButlast ++ shouldTestElementAt)
+    cs@(Counts _ _ errs fails) <- runTestTT $ TestList ( shouldTestMylast ++ shouldTestMyButlast ++ shouldTestElementAt ++ shouldTestmyLength ++ shouldTestmyReverse)
     putStrLn (showCounts cs)
     if (errs > 0 || fails > 0)
         then exitFailure

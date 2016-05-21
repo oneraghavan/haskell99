c5ace9ec6d61394cc6826661b3c54e7e191c9478
Raghavan <oneraghavan@gmail.com>
2016-05-21 11:36:27 +0530

    Added solutions for problems 12,13,14,15

--- haskell99/src/Problems10to20.hs
+++ haskell99/src/Problems10to20.hs
@@ -10,3 +10,22 @@ encodeModified (x:xs) =
                    _ | (l == 1) -> [Single x]
                      | (l > 1) -> [Multiple l x]
                   ++ encodeModified (filter (/= x) xs)
+
+decodeModified :: [Encoded a] -> [a]
+decodeModified [] = []
+decodeModified (Single x : xs) = [x] ++ decodeModified xs
+decodeModified (Multiple 2 x: xs) = [x] ++ decodeModified ((Single x):xs)
+decodeModified (Multiple c x: xs) = [x] ++ decodeModified ((Multiple (c -1) x):xs)
+
+
+-----------------PROBLEM 13 SOLVED----------------------------
+
+dupli :: [a] -> [a]
+dupli [] = []
+dupli (x:xs) = [x,x] ++ dupli xs
+
+repli :: [a] -> Int -> [a]
+repli [] c = []
+repli (x:[]) 1 = [x]
+repli (x:[]) c = [x] ++ (repli [x] (c - 1))
+repli (x:xs) c = (repli [x] c) ++ (repli (xs) c)
--- haskell99/test/TestProblem10to20.hs
+++ haskell99/test/TestProblem10to20.hs
@@ -11,7 +11,25 @@ import System.Exit
 
 shouldTestEncodeModified =
         [
-          TestCase  ( assertEqual "should pack the array" [Multiple 6 'a',Single 'b',Multiple 2 'c', Single 'd',Multiple 4 'e'] (encodeModified "aaaabccaadeeee" ))
+          TestCase  ( assertEqual "should encode the array" [Multiple 6 'a',Single 'b',Multiple 2 'c', Single 'd',Multiple 4 'e'] (encodeModified "aaaabccaadeeee" ))
          ]
 
-testProblems10to20 = shouldTestEncodeModified
+shouldTestDecodeModified =
+        [
+          TestCase  ( assertEqual "should decode the array" "aaaaaabccdeeee" (decodeModified [Multiple 6 'a',Single 'b',Multiple 2 'c', Single 'd',Multiple 4 'e'] )),
+          TestCase  ( assertEqual "should decode the empty array" [] (decodeModified ([]::[Encoded Int]) ))
+         ]
+
+shouldTestDupli =
+        [
+          TestCase  ( assertEqual "should duplicate the array" [1,1,2,2,3,3] (dupli [1,2,3] )),
+          TestCase  ( assertEqual "should return empty when given array" [] (dupli ([]::[Int]) ))
+         ]
+
+shouldTestRepli =
+        [
+          TestCase  ( assertEqual "should pack the array" "aaabbbccc" (repli "abc" 3))
+         ]
+
+testProblems10to20 = shouldTestEncodeModified ++ shouldTestDecodeModified ++ shouldTestDupli ++
+                      shouldTestRepli
